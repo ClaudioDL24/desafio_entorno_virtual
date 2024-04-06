a3610b89699bc2af26dfd2ccc51d979600db3e17
Claudio Merino <union.lm@gmail.com>
2024-04-06 12:13:11 -0300

    mi primer commits

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/claudiomerino/ProyectosBootcamp/desafio_entorno_virtual/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
